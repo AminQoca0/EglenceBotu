1758769c848e46ad1abd93d97cd42c84cfd20f54
AminQoca0 <83512377+AminQoca0@users.noreply.github.com>
2021-05-07 13:11:23 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Rixnux Code') {
+    msg.reply('♥');
+  }
+});
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./cmd/', (err, files) => {
+if (err) console.error(err);
+console.log(`${files.length} komut yüklenecek.`);
+
+client.login('procces.env.TOKEN');
