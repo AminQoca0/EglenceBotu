f26621c2bdc33e487904120e102ce49fb2868318
AminQoca0 <83512377+AminQoca0@users.noreply.github.com>
2021-05-07 13:37:22 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,57 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'Rixnux Code') {
-    msg.reply('♥');
-  }
-});
+const ayarlar = serverConfig = require("./ayarlar.json");
+const moment = require('moment');
+const { Client, Util } = require('discord.js');
+const fs = require('fs');
+require('./util/eventLoader.js')(client);
+var prefix = ayarlar.prefix;
+const log = message => {console.log(`${message}`);};
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir('./komutlar/', (err, files) => {
 if (err) console.error(err);
-console.log(`${files.length} komut yüklenecek.`);
-
-client.login('procces.env.TOKEN');
-//Rixnux Code
+log(`${files.length} komut yüklenecek.`);
+files.forEach(f => {
+let props = require(`./komutlar/${f}`);
+log(`${props.help.name} Komutu Yüklendi.`);
+client.commands.set(props.help.name, props);
+props.conf.aliases.forEach(alias => {
+client.aliases.set(alias, props.help.name);});});});
+client.reload = command => {
+return new Promise((resolve, reject) => {try {
+delete require.cache[require.resolve(`./komutlar/${command}`)];
+let cmd = require(`./komutlar/${command}`);
+client.commands.delete(command);
+client.aliases.forEach((cmd, alias) => {
+if (cmd === command) client.aliases.delete(alias);});
+client.commands.set(command, cmd);
+cmd.conf.aliases.forEach(alias => {
+client.aliases.set(alias, cmd.help.name);});resolve();} catch (e) {reject(e);}});};
+client.load = command => {
+return new Promise((resolve, reject) => {try {
+let cmd = require(`./komutlar/${command}`);
+client.commands.set(command, cmd);
+cmd.conf.aliases.forEach(alias => {
+client.aliases.set(alias, cmd.help.name);});
+resolve();} catch (e) {
+reject(e);}});};
+client.unload = command => {
+return new Promise((resolve, reject) => {
+try {
+delete require.cache[require.resolve(`./komutlar/${command}`)];
+let cmd = require(`./komutlar/${command}`);
+client.commands.delete(command);
+client.aliases.forEach((cmd, alias) => {
+if (cmd === command) client.aliases.delete(alias);});
+resolve();} catch (e) {
+reject(e);}});};
+client.elevation = message => {
+if (!message.guild) {
+return;}
+let permlvl = 0;
+if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+if (message.author.id === ayarlar.sahip) permlvl = 4;
+return permlvl;};
+client.login(process.env.token);
